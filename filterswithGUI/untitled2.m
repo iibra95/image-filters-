6f90f43fa01db1e6e8923a28d7cd17ddcbc9703c
mahmoud ibahim <mahmoudibra95@gmail.com>
2017-06-21 22:58:45 +0200

    GUI to choose image filters

---
+++ filterswithGUI/untitled2.m
@@ -0,0 +1,155 @@
+function varargout = untitled2(varargin)
+% UNTITLED2 MATLAB code for untitled2.fig
+%      UNTITLED2, by itself, creates a new UNTITLED2 or raises the existing
+%      singleton*.
+%
+%      H = UNTITLED2 returns the handle to a new UNTITLED2 or the handle to
+%      the existing singleton*.
+%
+%      UNTITLED2('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in UNTITLED2.M with the given input arguments.
+%
+%      UNTITLED2('Property','Value',...) creates a new UNTITLED2 or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before untitled2_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to untitled2_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help untitled2
+
+% Last Modified by GUIDE v2.5 07-May-2017 19:46:23
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @untitled2_OpeningFcn, ...
+                   'gui_OutputFcn',  @untitled2_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before untitled2 is made visible.
+function untitled2_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to untitled2 (see VARARGIN)
+
+% Choose default command line output for untitled2
+handles.output = hObject;
+
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes untitled2 wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = untitled2_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+% --- Executes on button press in pushbutton1.
+function pushbutton1_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+[name_file,name_path] = uigetfile({'*.jpg';'*.png';},...
+    'Buka Gambar');
+if ~isequal (name_file,0)
+    handles.data1= imread(fullfile(name_path,name_file));
+    guidata(hObject,handles);
+    axes(handles.axes1);
+    imshow(handles.data1);
+else
+    return
+end
+% --- Executes on selection change in popupmenu1.
+function popupmenu1_Callback(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+gambar = handles.data1;
+idmode = get(hObject,'value');
+if idmode==1
+elseif idmode==2
+    axes(handles.axes2);
+    imshow(gambar);
+elseif idmode==3
+    axes(handles.axes2);
+    A=rgb2gray(gambar);
+    imshow(A);
+elseif idmode==4
+    axes(handles.axes2);
+    A = rgb2gray(gambar);
+    B = im2bw(A);
+    imshow(B);
+elseif idmode==5
+    axes(handles.axes2);
+    A = rgb2gray(gambar);
+    imhist(A);
+elseif idmode==6
+    axes(handles.axes2);
+    A=rgb2gray(gambar);
+    B = histeq(A);
+    imshow(B);
+elseif idmode==7
+    axes(handles.axes2);
+    A=rgb2gray(gambar);
+    open = strel('disk',5);
+    set = imopen(A,open);
+    imshow(set);
+elseif idmode==8
+    axes(handles.axes2);
+    A = rgb2gray(gambar);
+    B = strel('disk',10);
+    C = imclose(A,B);
+    imshow(C);
+elseif idmode==9
+    axes(handles.axes2);
+    A = rgb2gray(gambar);
+    B = strel('disk',11);
+    C = imerode(A,B);
+    imshow(C);
+end
+
+% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from popupmenu1
+
+    
+
+% --- Executes during object creation, after setting all properties.
+function popupmenu1_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to popupmenu1 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
